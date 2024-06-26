aec245abdb798f902390d0a3494c07619640bb10
Adam <aoverholtzer@users.noreply.github.com>
2024-06-26 14:15:40 -0700

    include closing paren in links
    also disable images

--- Sources/TreeSitterMarkdownInlineQueries/highlights.scm
+++ Sources/TreeSitterMarkdownInlineQueries/highlights.scm
@@ -34,6 +34,6 @@
 ; ")" not part of query because of
 ; https://github.com/nvim-treesitter/nvim-treesitter/issues/2206
 ; TODO: Find better fix for this
-(image ["!" "[" "]" "("] @punctuation.delimiter)
-(inline_link ["[" "]" "("] @punctuation.delimiter)
-(shortcut_link ["[" "]"] @punctuation.delimiter)
+; (image ["!" "[" "]" "("] @punctuation.delimiter)
+(inline_link ["[" "]" "(" ")"] @punctuation.delimiter)
+; (shortcut_link ["[" "]"] @punctuation.delimiter)
